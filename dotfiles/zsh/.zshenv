e6ac7d4452a1c0a923edf42fd91bd59a127e84d6
WorlDXXXXX <michaelgmx1@googlemail.com>
2026-01-07 22:26:56 +0100

    Added powerlevel10k config and fix broken nvim installation

--- dotfiles/zsh/.zshenv
+++
@@ -1,8 +0,0 @@
-export PATH="$HOME/.local/bin:$PATH"
-
-export DOCKER_HOST="unix://${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/podman/podman.sock"
-export PODMAN_COMPOSE_PROVIDER="podman-compose"
-
-if command -v mise >/dev/null 2>&1; then
-  eval "$(mise activate zsh)"
-fi
--- dotfiles/zsh/aliases.zsh
+++
@@ -1,8 +0,0 @@
-# eza statt ls
-alias ls='eza --icons=auto --group-directories-first'
-alias ll='eza -lh --icons=auto --group-directories-first'
-alias la='eza -lha --icons=auto --group-directories-first'
-alias l='eza -lh --icons=auto --group-directories-first'
-
-# optional: Baumansicht
-alias lt='eza --tree --level=2 --icons=auto'
